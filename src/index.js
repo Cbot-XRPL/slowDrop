f9008324e7443b6543db0ac076742cce0597eea3
Cody Ricketts <94261914+C6bOt@users.noreply.github.com>
2022-08-31 21:46:00 -0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,97 +1,113 @@
-//import xrpl
-const xrpl = require('xrpl');
+//IMPORT XRPL MODULE BY  npm install xrpl -----------------------------------------------------------------------
+const xrpl = require('xrpl'); 
+//IMPORT ISSUER AND AMOUNT OF TOKENS TO CHECK FOR -----------------------------------------------------------------------
+const {requiredHoldings,checkIssuer1,checkIssuer2,ignoreWallet} = require('../config.json')  
 //import module2
 const send = require('./send')
 
-const {requiredHoldings,checkIssuer1,checkIssuer2,ignoreWallet} = require('../config.json')     
 
-//signal issure check 1 is done
-let run1 = false;
 
-//start holder array
-holders = [];
-
-//count outputs
+//line counter
 let counter = 0;
-
-//your own address for sending to self in test run
+//log counter
+let logCounter = 0;
+//holder counter
+holders = [];
+//test account
 let test = ignoreWallet;
 
-// Wrap code in an async function so we can use await-------------------------------------------
-           async function main() {
-                             
-// Define the network client--------------------------------------------------------------------
-           const client = new xrpl.Client("wss://xrplcluster.com/")
-           await client.connect()
-                             
-// Custom code goes here-------------------------------------------------------------------
 
- //2/ xrpl request issuer trustlines -------------------------------------------------------------------     
-           const response1 = await client.request({
-               "id": 2,
-               "command": "account_lines",
-               // imput the issuer your want to check for trustlines
-               "account": checkIssuer1,
-               "ledger_index": "validated"
-             })
-               //filter trust lines
-               response1.result.lines.forEach((line) =>{
-                // instead of 0 put in the number of tokens you want to require user to have // if doning nfts xls14 leave 0
-                if (line.balance * -1 > requiredHoldings){
-                   //exclude your holding wallet
-                    if (line.account != ignoreWallet){
-                   //builds holder list
-                    holders.push(line.account) 
-                    
-                    }   
-                }
-               },run1 = true
-             )
- 
-           
-             
-  //2/ check a diff issuer and add to holder list-------------------------------------------------------------------
-        /*     if(run1){
-             const response2 = await client.request({
-                "id": 2,
-                "command": "account_lines",
-                // imput the issuer 2 your want to check for trustlines
-                "account": checkIssuer2,
-                "ledger_index": "validated"
-              })
+
+
+
+//run primary token search and return page marker
+const getMarker = async (marker) => {
+    // establish new connection
+    const client = new xrpl.Client('wss://xrplcluster.com/');
+    await client.connect();
+
+    const requestBody = {
+        id: 2,
+        command: 'account_lines',
+        //issuing account of the project -----------------------------------------------------------------------
+        account: checkIssuer1,
+        ledger_index: 'validated',
+    };
+
+    // add marker to the request obj
+    if (marker) requestBody.marker = marker;
+
+    // make request
+    const response = await client.request(requestBody);
+
+    // do some logic with response
+    response.result.lines.forEach((line) => {
+                      //how many tokens each wallet has -----------------------------------------------------------------------
+        if (line.balance * -1 >= requiredHoldings) {
             
-              response2.result.lines.forEach( line =>{
-                 if (line.balance * -1 > requiredHoldings ){
-                     if (line.account != ignoreWallet){
-                     holders.push(line.account);
-                    
-                     }
-                 } 
-              })   
-            } 
-  */
-  //2/ cycle holder list send tokens and log data-------------------------------------------------------------------
-            holders.reduce(async(memo, holder) =>{
-              await memo;
-              counter ++;
-                       
-       //3/test---------------------------------
-                   console.log(holder)
-                   await send(test, counter);
-      //3/airdrop-------------------------------
-                   //await send(holder, counter);
-                       
-              },undefined)
-
-           client.disconnect()
-                             
-            }; 
+            if (line.account != ignoreWallet){
+                //builds holder list
+                logCounter++
+                console.log(`holder ${logCounter} pushed to array`);
+                 holders.push(line) 
+                 }   
            
-          
-          
-           main()
-             
+        }
+    });
 
-             
+    // disconnect
+    client.disconnect();
 
-         
+// this returns next page marker
+    return response.result.marker;
+};
+
+
+
+//Define recursive function which calls itself if a page marker exists
+const recursive = async ( marker) => {
+    if (marker) {
+        getMarker(marker)
+        .then(async (nextMarker) => {
+        recursive( await nextMarker);
+        })
+        .catch((error) => console.log(error));
+    }else{
+     console.log('done making array')
+
+
+
+//2/ cycle holder list send tokens and log data-------------------------------------------------------------------
+holders.reduce(async(memo, holder) =>{
+    await memo;
+    counter ++;
+    
+
+//3/holder list---------------------------------
+         //console.log(holder, counter)
+//3/test---------------------------------
+        await send(test, counter);
+//3/airdrop-------------------------------
+         //await send(holder, counter);
+
+    },undefined)
+
+
+
+
+    }
+};
+
+
+
+// call get marker with null then run recursive that stops when it runs out of pages
+getMarker().then(async(nextMarker)=>{
+await recursive(nextMarker)
+})
+
+
+ 
+
+
+
+
